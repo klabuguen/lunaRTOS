6d28fbae64bfdbe105c9392a661ccfebda26f0e3
Katarina Labuguen <klabugue@gmail.com>
2024-12-03 15:34:15 -0800

    feat: implementation of simple timebase utility

--- drivers/Src/main.c
+++ drivers/Src/main.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
 #include "led.h"
+#include "uart.h"
+#include "timebase.h"
 
 int main(void){
+	timebase_init();
 	led_init();
+	uart_tx_init();
 	while(1){
-		led_on();
-		for(int i = 0; i < 100000; i++){}
-		led_off();
-		for(int i = 0; i < 100000; i++){}
+		 led_on();
+		 printf("led on\n\r");
+		 delay(1);
+		 led_off();
+		 printf("led off\n\r");
+		 delay(1);
 	}
 
 }
