bbe63b7e55f04e20946fe1c2eabb31a51358e82e
Katarina Labuguen <klabugue@gmail.com>
2024-12-19 20:32:52 -0800

    feat: example program using LunaRTOS round robin and cooperative scheduling

--- drivers/Src/main.c
+++ drivers/Src/main.c
@@ -1,20 +1,97 @@
 #include <stdio.h>
-#include <stdint.h>
 #include "led.h"
 #include "uart.h"
-#include "timebase.h"
+#include "kernel.h"
 
-int main(void){
-	timebase_init();
-	led_init();
-	uart_tx_init();
-	while(1){
-		 led_on();
-		 printf("led on\n\r");
-		 delay(1);
-		 led_off();
-		 printf("led off\n\r");
-		 delay(1);
+#define QUANTA	10
+
+typedef uint32_t TaskProfiler;
+
+TaskProfiler Task0_Profiler = 0, Task1_Profiler = 0,Task2_Profiler = 0;
+
+void motor_run(void);
+void motor_stop(void);
+void valve_open(void);
+void valve_close(void);
+
+
+void task0(void)
+{  
+	while(1)
+	{
+		Task0_Profiler++;
+		ThreadYield();
+//		motor_run();
+
 	}
+}
+
+
+void task1(void)
+{
+	while(1)
+	{
+		Task1_Profiler++;
+		ThreadYield();
+//		valve_open();
+	}
+}
+
+void task2(void)
+{
+	while(1)
+	{
+		Task2_Profiler++;
+		ThreadYield();
+//		motor_stop();
+	}
+}
+
+void task3(void){
+	while(1){
+
+	}
+}
+
+int main(void)
+{
+
+
+	uart_tx_init();
+
+	/*Initialize Kernel*/
+	KernelInit();
+	/*Add Threads*/
+	KernelCreateThreads(&task0,&task1,&task2);
+
+	/*Set RoundRobin time quanta*/
+	KernelLaunch(QUANTA);
+
 
 }
+
+
+void motor_run(void)
+{
+	printf("Motor is starting...\n\r");
+
+}
+
+
+void motor_stop(void)
+{
+	printf("Motor is stopping...\n\r");
+}
+
+void valve_open(void)
+{
+	printf("Valve is opening...\n\r");
+}
+
+
+void valve_close(void)
+{
+	printf("Valve is closing...\n\r");
+}
+
+
